0a032aef6c9cc33306f99399b9ee4078a1e887e9
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-10-30 09:39:31 -0300

    Formulario de dados pessoais

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,33 @@ function App() {
         {View}
       </Center>
 
+      <Center>
+        <Grid autoColumns h={400} >
+          <GridItem bg={"#fff"} mr={20} colSpan={2} h="100%" w={400} borderRadius={40}>
+            {/* FORM */}
+            <Center flexDirection={"column"}>
+              <Text fontSize='4xl' >
+                Dados Pessoais
+              </Text>
+              <Center >
+                <FormControl w={240}>
+                  <FormLabel>Nome</FormLabel>
+                  <Input isInvalid={isErrorName} type='text' value={name} onChange={handleNameChange} />
+                  <FormLabel fontSize={20}>Email</FormLabel>
+                  <Input isInvalid={isErrorEmail} type='text' value={email} onChange={handleEmailChange} />
+                  <FormLabel fontSize={20}>Telefone</FormLabel>
+                  <Input type='tel' isInvalid={isErrorTelefone} value={telefone} onChange={handleTelefoneChange} />
+                  <FormLabel fontSize={20}>CPF</FormLabel>
+                  <Input type='number' isInvalid={isErrorCpf} value={cpf} onChange={handleCPFChange} />
+                </FormControl>
+              </Center>
+            </Center>
+          </GridItem>
+
+
+        </Grid>
+      </Center>
+
     </Flex >
 
 
