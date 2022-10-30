324620daa41f99e5dcd26d773e8f85564eee106b
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2022-10-30 09:31:18 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {
+  Center, Flex, Grid, GridItem, Text, Button, Checkbox, Stack, FormControl, FormLabel, Input,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useLottie } from 'lottie-react'
+import api from './services/api';
+import plane from './assets/plane.json'
+
+function App() {
+
+  return (
+    <Flex height="100vh" bg={"#3490dc"} w={"100%"} flexDirection={"column"}  >
+
+
+    </Flex >
+
+
+
+  )
+}
+
+export default App
